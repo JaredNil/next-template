697b2bc762c14487af1efa45cd74c4ac1f813a21
JaredN <77531492+JaredNil@users.noreply.github.com>
2025-07-27 15:53:17 +0800

    sb-init

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import { globalIgnores } from 'eslint/config';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -31,4 +34,4 @@ export default tseslint.config([
   },
   eslintBoundariesConfig,
   eslintImportConfig,
-]);
+], storybook.configs["flat/recommended"]);
--- src/shared/theme/index.ts
+++ src/shared/theme/index.ts
@@ -8,7 +8,8 @@ export const useTheme = () => {
   useEffect(() => {
     const root = window.document.documentElement;
 
-    root.setAttribute('data-theme', theme);
+    root.classList.remove('light', 'dark');
+    root.classList.add(theme);
     localStorage.setItem('theme', theme);
   }, [theme]);
 
